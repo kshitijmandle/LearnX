b846630f84f4cfeb9b488271b33b173e0a379f44
kshitij <kshitijmandle703044@gmail.com>
2021-11-27 20:12:56 +0530

    Initial commit

---
+++ app/src/main/java/com/learn/learnx/MainActivity.java
@@ -0,0 +1,14 @@
+package com.learn.learnx;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
