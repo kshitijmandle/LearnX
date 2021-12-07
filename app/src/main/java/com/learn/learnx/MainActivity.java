9ab7cc633ae0ab6abe6d9e24105fbaa9ecb010bc
kshitij <kshitijmandle703044@gmail.com>
2021-12-07 15:58:55 +0530

    Home Activity and Toolbar Add

--- app/src/main/java/com/learn/learnx/MainActivity.java
+++ app/src/main/java/com/learn/learnx/MainActivity.java
@@ -1,9 +1,14 @@
 package com.learn.learnx;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.learn.learnx.App_Activities.HomePage;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +17,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "Awesome", Toast.LENGTH_SHORT).show();
+        TextView hello = findViewById(R.id.hello);
+        hello.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,HomePage.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
